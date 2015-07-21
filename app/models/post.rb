42b93eead4d520829b7bfbfa47266781225c7ccc
Nicolas Feller <nrkfeller@gmail.com>
2015-07-21 21:47:30 +0000

    navigation in nav bar and side bar

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,4 @@ class Post < ActiveRecord::Base
     validates :title, presence: true, length: {minimum: 5}
     validates :body, presence: true
     has_many :comments, dependent: :destroy
-    
 end
